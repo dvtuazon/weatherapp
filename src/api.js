740e159dd1cdd252d79c8302908059aa515ad8da
David <dvtuazon@gmail.com>
2022-01-17 02:32:59 -0800

    cors issue fixed

--- src/api.js
+++ src/api.js
@@ -3,17 +3,13 @@ import showWeather from './components/WeatherComponent';
 function fetchWeather(location) {
     console.log(location);
     const key = "45ee0388848fe31389ddda5caa15726b";
-    // let url = `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
 
-    fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+    fetch(url)
     .then(res => {
         if (!res.ok) throw new Error(res.statusText);
-        return res.json();
+        let resjson = res.json();
+        return resjson;
     })
     .then(data => {
         console.log(data);
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -10,21 +10,23 @@ function Search() {
         fetchWeather(location);
     }
     return (
-        <Container className='center'>
+        <div id='video-bg'>
+        <Container className='vertical-center'>
             <Form onSubmit={e => { handleSubmit(e) }}>
                 <Row className='justify-content-center'>
                     <Col xs={10} sm={8} lg={6}>
-                        <Label for='search' className='d-block text-center my-3'>Weather Search</Label>
+                        <Label for='search' className='d-block text-center my-3 search-text'>Weather Search</Label>
                         <Input type='text' name='search' id='search' placeholder='Enter location' value={location} onChange={e => setLocation(e.target.value)} />
                     </Col>
                 </Row>
                 <Row className='my-4 justify-content-center'>
                     <Col xs={5} md={2} lg={2}>
-                        <Input type='submit' value='Go' />
+                        <Input type='submit' value='Go' className='btn btn-primary' />
                     </Col>
                 </Row>
             </Form>
         </Container>
+        </div>
     )
 }
 
